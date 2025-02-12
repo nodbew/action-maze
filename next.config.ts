c0aba2189ba32d4fe2436ab2cdae66b62d1bdad4
nodbew <165175929+nodbew@users.noreply.github.com>
2025-02-12 04:17:37 +0000

    Update credits page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rewrites: async () => [
+    {
+      source: "/credits",
+      destination: "/credits.html"
+    }
+  ]
 };
 
 export default nextConfig;
